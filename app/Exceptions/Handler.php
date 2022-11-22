33ba9d89f4d38860e417bd8e540bce9e622cd15b
Joe Lee <eojeel@users.noreply.github.com>
2022-11-22 20:50:41 +0000

    error handeling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Inertia\Inertia;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -47,4 +48,27 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+ * Prepare exception for rendering.
+ *
+ * @param  \Throwable  $e
+ * @return \Throwable
+ */
+public function render($request, Throwable $e)
+{
+    $response = parent::render($request, $e);
+
+    if (in_array($response->status(), [500, 503, 404, 403])) {
+        return Inertia::render('Error', ['status' => $response->status()])
+            ->toResponse($request)
+            ->setStatusCode($response->status());
+    } else if ($response->status() === 419) {
+        return back()->with([
+            'message' => 'The page expired, please try again.',
+        ]);
+    }
+
+    return $response;
+}
 }
---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,42 @@
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    status: ''
+})
+
+const title = computed(() => {
+            return {
+                503: 'Service Unavailable',
+                500: 'Server Error',
+                404: 'Page Not Found',
+                403: 'Forbidden',
+            }[this.status]
+        });
+
+const description = computed(() => {
+            return {
+                503: 'Sorry, we are doing some maintenance. Please check back soon.',
+                500: 'Whoops, something went wrong on our servers.',
+                404: 'Sorry, the page you are looking for could not be found.',
+                403: 'Sorry, you are forbidden from accessing this page.',
+            }[this.status]
+        });
+</script>
+
+
+<template>
+    <div class="flex justify-center items-center min-h-full h-full w-full min-w-full">
+        <div class="text-indigo-600 text-5xl px-8 font-bold border-r pb-8">
+            {{ this.status }}
+        </div>
+        <div class="px-8">
+            <h1 class="text-5xl font-bold mb-4">{{ title }}</h1>
+            <p class="text-gray-600">{{ description }}</p>
+            <div>
+                <button>Go Home</button>
+            </div>
+        </div>
+    </div>
+</template>
