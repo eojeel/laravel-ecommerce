d7902cad3bcb132731ee6731799a886e70ed18b4
joe lee <eojeel@users.noreply.github.com>
2023-02-01 16:50:11 +0000

    order page update

--- resources/js/Pages/Order/Index.vue
+++ resources/js/Pages/Order/Index.vue
@@ -28,10 +28,12 @@ const props = defineProps({
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="border-b">
+                        {{  orders.orders.data  }}
+                        <tr v-for="(order, index) of orders.data" class="border-b">
                             <td>
+                                {{  order }}
                                 <a href="/src/order-details.html" class="text-purple-600 hover:text-purple-500">
-                                    #1234
+                                    #{{ order.id }}
                                 </a>
                             </td>
                             <td>May 3, 07:28PM</td>
@@ -96,132 +98,31 @@ const props = defineProps({
                                 </button>
                             </td>
                         </tr>
-                        <tr class="border-b">
-                            <td>
-                                <a href="/src/order-details.html" class="text-purple-600 hover:text-purple-500">
-                                    #1234
-                                </a>
-                            </td>
-                            <td>May 3, 07:28PM</td>
-                            <td>
-                                <span class="bg-emerald-500 text-white p-1 rounded">Paid</span>
-                            </td>
-                            <td>$58.25</td>
-                            <td class="flex gap-3">
-                                <button
-                                    class="btn-primary bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                                    </svg>
-                                    Invoice
-                                </button>
-                                <button class="btn-primary py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                                    </svg>
-                                    Pay
-                                </button>
-                            </td>
-                        </tr>
-                        <tr class="border-b">
-                            <td>
-                                <a href="/src/order-details.html" class="text-purple-600 hover:text-purple-500">
-                                    #1234
-                                </a>
-                            </td>
-                            <td>May 3, 07:28PM</td>
-                            <td>
-                                <span class="bg-orange-300 text-white p-1 rounded">Processing</span>
-                            </td>
-                            <td>$58.25</td>
-                            <td class="flex gap-3">
-                                <button
-                                    class="btn-primary bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                                    </svg>
-                                    Invoice
-                                </button>
-                                <button class="btn-primary py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                                    </svg>
-                                    Pay
-                                </button>
-                            </td>
-                        </tr>
-                        <tr class="border-b">
-                            <td>
-                                <a href="/src/order-details.html" class="text-purple-600 hover:text-purple-500">
-                                    #1234
-                                </a>
-                            </td>
-                            <td>May 3, 07:28PM</td>
-                            <td>
-                                <span class="bg-emerald-500 text-white p-1 rounded">Paid</span>
-                            </td>
-                            <td>$58.25</td>
-                            <td class="flex gap-3">
-                                <button
-                                    class="btn-primary bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                                    </svg>
-                                    Invoice
-                                </button>
-                                <button class="btn-primary py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                                    </svg>
-                                    Pay
-                                </button>
-                            </td>
-                        </tr>
-                        <tr class="border-b">
-                            <td>
-                                <a href="/src/order-details.html" class="text-purple-600 hover:text-purple-500">
-                                    #1234
-                                </a>
-                            </td>
-                            <td>May 3, 07:28PM</td>
-                            <td>
-                                <span class="bg-red-500 text-white p-1 rounded">Cancelled</span>
-                            </td>
-                            <td>$58.25</td>
-                            <td class="flex gap-3">
-                                <button
-                                    class="btn-primary bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                                    </svg>
-                                    Invoice
-                                </button>
-                                <button class="btn-primary py-1 px-2 flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                                    </svg>
-                                    Pay
-                                </button>
-                            </td>
-                        </tr>
+
+
                     </tbody>
                 </table>
+
+                <!---
+                <span class="bg-red-500 text-white p-1 rounded">Cancelled</span>
+                <span class="bg-emerald-500 text-white p-1 rounded">Paid</span>
+                <span class="bg-orange-300 text-white p-1 rounded">Processing</span>
+                <span class="bg-gray-500 text-white p-1 rounded">Unpaid</span>
+                -->
+                <nav v-if="orders.total > 0"
+                        class="relative z-0 inline-flex justify-center rounded-md shadown-sm -space-x-pn"
+                        aria-label="Pagination">
+                        <a v-for="(link, i) of orders.links" :key="i" :disabled="!link.url" :href="link.url"
+                            @click.prevent="getForPage($event, link)" aria-current="page"
+                            class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+                            :class="[
+                                link.active
+                                    ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                                    : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+                                i === 0 ? 'rounded-lg-md' : '',
+                                i === orders.links.length - 1 ? 'rounded-r-md' : ''
+                            ]" v-html="link.label"></a>
+                    </nav>
             </div>
         </div>
     </DefaultLayout>
